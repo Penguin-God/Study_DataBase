6d1dac0495ca54e455ebd4bb3286235a2c329acc
Penguin-God <gkswh4860@naver.com>
2021-09-10 19:54:40 +0900

    설명 내용 표시

--- Assets/Script/SlotManager.cs
+++ Assets/Script/SlotManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class SlotManager : MonoBehaviour
 {
     public string currentTabName = "Character";
-    public List<ItemData> showItemList = new List<ItemData>();
+    public List<ItemData> currentItemList = new List<ItemData>();
 
     [SerializeField] GameObject[] slots = null;
     private void Start()
@@ -19,7 +19,7 @@ public class SlotManager : MonoBehaviour
         currentTabName = tabName;
         // 일치하는 값뿐만 아니라 람다식을 사용해서 조건에 맞는 값들도 매서드를 통해 가져올 수 있음
         // 여기서 가져온 리스트는 같은 주소값을 참조함 (Return한 리스트의 값을 바꾸면 원본 리스트의 값도 바뀌고 반대도 됨)
-        showItemList = DataBaseManager.instance.MyItemList.FindAll(itemData => itemData.type == tabName);
+        currentItemList = DataBaseManager.instance.MyItemList.FindAll(itemData => itemData.type == tabName);
 
         UpdateSlot();
         SetTabSprite();
@@ -46,9 +46,9 @@ public class SlotManager : MonoBehaviour
 
     public void ClickSlot(int number)
     {
-        // 참조값이 같아 current_Item의 값 수정이 showItemList -> MyItemList로 전달됨 (new 쓰면 참조값이 달라져서 작동 안됨)
-        ItemData current_Item = showItemList[number];
-        ItemData using_Item = showItemList.Find(data => data.isUsing);
+        // 참조값이 같아 current_Item의 값 수정이 currentItemList -> MyItemList로 전달됨 (new 쓰면 참조값이 달라져서 작동 안됨)
+        ItemData current_Item = currentItemList[number];
+        ItemData using_Item = currentItemList.Find(data => data.isUsing);
 
         if(currentTabName == "Character")
         {
@@ -70,25 +70,51 @@ public class SlotManager : MonoBehaviour
     [SerializeField] Image[] arr_ItemImage = null;
     public void UpdateSlot()
     {
-        for (int i = 0; i < showItemList.Count; i++)
+        for (int i = 0; i < currentItemList.Count; i++)
         {
             slots[i].SetActive(true);
-            arr_UsingImage[i].SetActive(showItemList[i].isUsing);
-            slots[i].GetComponentInChildren<Text>().text = showItemList[i].name;
+            arr_UsingImage[i].SetActive(currentItemList[i].isUsing);
+            slots[i].GetComponentInChildren<Text>().text = currentItemList[i].name;
             // MyItemList에서 인덱스를 이미지에 해당하는 인덱스를 찾아 MyItemList과 크기가 같은 스프라이트 배열에 대입
-            arr_ItemImage[i].sprite = arr_ItemSprite[DataBaseManager.instance.MyItemList.FindIndex(data => data.name == showItemList[i].name)];
+            arr_ItemImage[i].sprite = arr_ItemSprite[DataBaseManager.instance.MyItemList.FindIndex(data => data.name == currentItemList[i].name)];
         }
     }
 
-    [Space][Space]
+    [Space] [Space]
+    [SerializeField] RectTransform canvasRect = null;
     [SerializeField] GameObject explainPanel = null;
+    [SerializeField] Vector2 distance_PanelBetweenMouse = Vector2.zero;
     Coroutine Co_PointerEnter = null;
     public void PointerEnter(int number) => Co_PointerEnter = StartCoroutine(PointerEnterDelay(number));
 
     IEnumerator PointerEnterDelay(int number)
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.1f);
+        SetPanel(number);
         explainPanel.SetActive(true);
+
+        RectTransform panelRect = explainPanel.GetComponent<RectTransform>();
+        while (true)
+        {
+            // 마우스 포지션에 사용되는 좌표계( (0,0)부터 해상도 크기까지 값을 가지는 사각형)를 Canvas좌표계(가운데가 (0,0)인 사각형 좌표계)로 바꾸는 작업
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, Input.mousePosition, Camera.main, out Vector2 panelPos);
+            panelRect.anchoredPosition = panelPos + distance_PanelBetweenMouse;
+            yield return null;
+        }
+    }
+
+
+    [SerializeField] Text txt_ItemName = null;
+    [SerializeField] Text txt_ItemCount= null;
+    [SerializeField] Text txt_ItemExplain = null;
+    [SerializeField] Image img_Panel = null;
+    void SetPanel(int slotNum)
+    {
+        txt_ItemName.text = currentItemList[slotNum].name;
+        txt_ItemCount.text = currentItemList[slotNum].number + "개";
+        txt_ItemExplain.text = currentItemList[slotNum].explain;
+        img_Panel.sprite = arr_ItemSprite[DataBaseManager.instance.MyItemList.FindIndex(data => data.name == currentItemList[slotNum].name)];
+
     }
 
     public void PointerEXit(int number)
@@ -96,4 +122,5 @@ public class SlotManager : MonoBehaviour
         StopCoroutine(Co_PointerEnter);
         explainPanel.SetActive(false);
     }
+
 }
