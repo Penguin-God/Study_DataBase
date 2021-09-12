477a4002e208ae3187499fdaeb632bd4e5291e24
Penguin-God <gkswh4860@naver.com>
2021-09-12 19:32:18 +0900

    아이템 초기화 진행중

---
+++ Assets/Script/ItemManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemManager : MonoBehaviour
+{
+    [SerializeField] InputField input_ItemName;
+    [SerializeField] InputField input_ItemCount;
+
+    public void GetItemClcik() 
+    {
+
+    }
+
+    public void RemoveItemClcik()
+    {
+
+    }
+}
