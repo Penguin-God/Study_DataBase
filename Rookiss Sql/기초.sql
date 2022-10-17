79b0bc4199924e8d839778fff1d1a30093cc5ce4
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-10-17 17:38:12 +0900

    sql 2, 3, 4강

--- Rookiss Sql/기초.sql
+++ Rookiss Sql/기초.sql
@@ -1,4 +1,17 @@
-SELECT *
-FROM dbo.managers;
--- 주석
-/* 주석 */
+--ORDER BY 
+--SELECT TOP 10 nameFirst, nameLast, birthYear, birthMonth, birthDay
+--FROM players WHERE birthYear IS NOT NULL ORDER BY birthYear DESC, birthMonth DESC, birthDay DESC;
+
+-- birthCity LIKE 'NEW YOR_'; : LIKE를 이용한 문자열 패턴 매칭.
+
+
+-- 연산
+SELECT 2023 - birthYear AS KoearAge 
+FROM players 
+WHERE deathYear IS NULL and birthYear IS NOT NULL AND 2023 - birthYear <= 70
+ORDER BY KoearAge;
+
+-- 문자열 연산 예제
+SELECT nameFirst + ' ' + nameLast AS fullName
+FROM players
+WHERE nameFirst IS NOT NULL AND nameLast IS NOT NULL;
