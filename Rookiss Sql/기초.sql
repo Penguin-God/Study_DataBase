bd72ecc070dba4255fa9a448a9cb06af20c780e3
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-10-28 20:34:55 +0900

    윈도우 함수

--- Rookiss Sql/기초.sql
+++ Rookiss Sql/기초.sql
@@ -1,16 +1,6 @@
 USE BaseballData
 
-DECLARE @testTable TABLE
-(
-	name VARCHAR(50) NOT NULL,
-	salary INT NOT NULL
-)
-
-INSERT INTO @testTable
-SELECT pl.nameFirst + ' ' + pl.nameLast, sa.salary
-FROM players AS pl
-	INNER JOIN salaries AS sa
-	ON pl.playerID = sa.playerID
-
-SELECT *
-FROM @testTable
+SELECT *,
+	RANK() OVER (PARTITION BY playerID ORDER BY salary DESC)
+FROM salaries 
+ORDER BY playerID
